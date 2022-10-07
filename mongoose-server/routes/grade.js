488b49a8f70b5472e5def345a1bf67e15d447ea9
zhoushiming <marchmi@163.com>
2022-10-07 11:50:32 +0800

    新增aggregate方法用例

--- mongoose-server/routes/grade.js
+++ mongoose-server/routes/grade.js
@@ -6,11 +6,53 @@ const tools = require('../utils/tools')
 
 router.get('/list', async(req, response, next) => {
     try {
-        const filter = {},
-              query = req.query,
-              limit = parseInt(query.pageSize),
-              skip = (query.pageNum - 1) * limit
-    } catch (error) {
+        const filter = {
+            // gradeCode: {
+            //     $gte: 2022 // gradeCode一开始设定为String类型，条件操作符$gte则无法生效  https://blog.csdn.net/qq_43472877/article/details/110823207
+            // },
+            // gradeName: /2022/ // 模糊匹配
+        },
+            query = req.query,
+            limit = parseInt(query.pageSize),
+            skip = (query.pageNum - 1) * limit
+        const { body, baseUrl, path } = req
+        client.updateSchema(tools.replacePathLine(baseUrl),{ timestamps: true }).updateModel()
+        const result = await client.aggregate(
+            {
+                $match: filter  // 匹配条件
+            },
+            // {
+            //     $group: {
+            //         _id: '$gradeCode',   // 根据gradeCode这个字段的值进行分组，https://blog.csdn.net/qq_18948359/article/details/88777066
+            //         count: {
+            //             $sum: 1          // 统计数据
+            //         }
+            //     }
+            // },
+            {
+                $project: {     // 控制查询出来的字段
+                    _id: 1,
+                    // count: 1,// 配置$group属性并配置该字段时才生效
+                    gradeName: 1,// 配置了$group属性时，没有在$group中声明的字段不会生效
+                    gradeCode: 1,
+                    describe: 1
+                }
+            },
+            {
+                $sort: { gradeCode : 1 } // 根据gradeCode字段进行排序 1升序，-1降序
+            },
+            {
+                $skip:1     // 跳过一定数据
+            },
+            {
+                $limit:1    // 查询数据的条数
+            },
+            // {
+            //     $unwind:'$tags' 将数组类型的值，按元素拆分成多条独立的数据进行返回
+            // }
+        )
+        response.send(result)
+} catch (error) {
         console.log(error)
         next(error)
     }

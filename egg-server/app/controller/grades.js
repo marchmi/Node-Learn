53bf99266b56db2e5e9285f9c71af5dcda90b3a0
zhoushiming <marchmi@163.com>
2022-10-10 16:23:20 +0800

    RESTful 风格的URL定义

---
+++ egg-server/app/controller/grades.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { Controller } = require('egg');
+
+/**
+ * RESTful 风格的 URL与方法对照表
+ * method     path                routeName     controller.action
+ * GET        /grades             gardes        controller.grades.index   // 列表
+ * GET        /grades/new         new_grade     controller.grades.new
+ * GET        /grades/:id         grade         controller.grades.show    // 详情
+ * GET        /grades/:id/edit    edit_grade    controller.grades.edit
+ * POST       /grades             grades        controller.grades.create  // 新增
+ * PUT        /grades/:id         grade         controller.grades.update  // 更新
+ * DELETE     /grades/:id         grade         controller.grades.destroy // 删除
+ */
+class GradesController extends Controller {
+  async index() {
+    const { ctx } = this; // ctx.query get请求参数 ctx.request.body post请求提交参数
+    ctx.body = `${JSON.stringify(ctx.query)}`;
+  }
+
+  async show() {
+    const { ctx } = this;
+    ctx.body = `${JSON.stringify(ctx.params)}`;
+  }
+
+  async create() {
+    const { ctx } = this;
+    ctx.body = `${JSON.stringify(ctx.request.body)}`;
+  }
+
+  async update() {
+    const { ctx } = this;
+    ctx.body = `${JSON.stringify(ctx.request.body)}`;
+  }
+
+  async destroy() {
+    const { ctx } = this;
+    ctx.body = `${JSON.stringify(ctx.request.body)}`;
+  }
+}
+
+module.exports = GradesController;
--- egg-server/app/router.js
+++ egg-server/app/router.js
@@ -8,4 +8,7 @@ module.exports = app => {
   // 引入和使用路由级别的中间件
   const part = app.middleware.part(); // 单参数则为app.middleware.part(options, app)
   router.get('/', part, controller.home.index);
+
+  router.resources('grades', '/api/grades', controller.grades); // RESTful 风格的 URL 定义
+  // app.router.get('/grades/:id/:name', controller.grades.info); // 多个参数
 };

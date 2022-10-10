cd742e9ef85a588ff0e619f5198a6b7df3e2a3bf
zhoushiming <marchmi@163.com>
2022-10-10 14:49:50 +0800

    路由级别中间件处理

---
+++ egg-server/app/middleware/part.js
@@ -0,0 +1,13 @@
+/**
+ * 局部的Part中间件，只在部分路由中调用
+ * @return empty
+ */
+module.exports = () => {
+  return async function part(ctx, next) {
+    console.log('----------------------------------');
+    console.log('局部的Part中间件');
+    console.log('----------------------------------');
+    await next();
+  };
+};
+
--- egg-server/app/router.js
+++ egg-server/app/router.js
@@ -5,5 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  // 引入和使用路由级别的中间件
+  const part = app.middleware.part(); // 单参数则为app.middleware.part(options, app)
+  router.get('/', part, controller.home.index);
 };
